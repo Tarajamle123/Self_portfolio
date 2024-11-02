bb55d2506b72e02946f3414b3b9b8af5d010a41e
Tara jamle <162895006+Tarajamle123@users.noreply.github.com>
2024-11-03 01:15:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-
-  document.getElementById("menu-icon").onclick = function() {
-    document.querySelector(".navlist").classList.toggle("active");
-};
-
-
-
+  document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    alert("Message sent successfully!");
+  });
 const header = document.querySelector("header");
 const menu = document.querySelector("#menu-icon");
 const navlist = document.querySelector('.navlist');
 
 // Adding scroll event to window
-window.addEventListener("scroll", function() {
-    // Toggle the 'sticky' class based on the scroll position
+  window.addEventListener("scroll", function() {
+     // Toggle the 'sticky' class based on the scroll position
     header.classList.toggle("sticky", this.scrollY > 120);
     
     // If the page is scrolled, remove the active class from navlist and toggle class from menu
@@ -26,4 +23,4 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x'); // Toggle the icon for the menu
     navlist.classList.toggle('active'); // Toggle the visibility of the navlist
 };
-
+  
